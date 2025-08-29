992fee645745e8c31bee918185f3b480a9c9565b
ProjectDelanoysTecnoIA <delanoystorreshever@gmail.com>
2025-08-29 12:21:02 -0500

    Ruta de Aprendizaje - Proyecto Web Completo- Agenda

---
+++ JavaScrpt/rutaproyectoweb.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const checkboxes = document.querySelectorAll("input[type=checkbox]");
+  const progreso = document.getElementById("progreso");
+  const porcentaje = document.getElementById("porcentaje");
+
+  function actualizarProgreso() {
+    const total = checkboxes.length;
+    const marcados = document.querySelectorAll("input[type=checkbox]:checked").length;
+    const percent = Math.round((marcados / total) * 100);
+
+    progreso.style.width = percent + "%";
+    porcentaje.textContent = percent + "%";
+  }
+
+  checkboxes.forEach(chk => {
+    chk.addEventListener("change", actualizarProgreso);
+  });
+
+  actualizarProgreso();
+});
