f591d22ed136836cbe4c5fee70401b61cd7f9576
Hever Torres <delanoystorreshever@gmail.com>
2026-02-16 21:49:58 -0500

    slider de aires

--- js/airesylavadoras.js
+++ js/airesylavadoras.js
@@ -15,3 +15,41 @@ document.addEventListener("DOMContentLoaded", () => {
         observer.observe(card);
     });
 });
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Control de siguiente/atrás
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+// Control de puntos
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+    
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
+
+// Cambio automático (opcional)
+setInterval(() => {
+    changeSlide(1);
+}, 5000);
